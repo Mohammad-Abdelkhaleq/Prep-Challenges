6d249c23820f0aba27def558052a16517dcab041
Mohammad-Abdelkhaleq <alamrimohammad959@gmail.com>
2023-04-09 08:51:01 +0000

    finished

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
-const reverseArray = (arr)=>{
-    //     // write your code here
-    //     let reverseArr=[];
-    //     for(let i=arr.length-1 ; i>=0 ; i-- ){
-    //         reverseArr.push(arr[i]);
-    //     }
-    //     return reverseArr
-    // // }
-    
-    // another solution 
-       let revArr=[];
-       for(let i=0;i<5;i++){
-        let x=arr.pop()
-        console.log(x)
-        revArr.push(x)
-       }
-       return revArr
+const objLat = (obj) => {
+   // write your code here
+   return `my name is ${obj.firstName} ${obj.lastName} I am ${obj.age} YO, and I love ${obj.hobby}.`
+};
+let obj = {
+        firstName: 'Ellie',
+        lastName: 'jon',
+        age: 67,
+        hobby: 'Gaming and Sleeping'
     }
-   console.log( reverseArray([1,2,3,4,5]));;
+    console.log(obj);
